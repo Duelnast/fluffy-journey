01e395e57071f7b8dd1181d54a8670fc67b49192
Duelnast <logolenarczyk@gmail.com>
2025-06-12 20:23:59 +0000

    -ogarniecie lokalizacji folderu projektu

--- src/connections/data_fetch.py
+++ src/connections/data_fetch.py
@@ -1,12 +1,14 @@
 from dotenv import load_dotenv
 from pathlib import Path
+from src.utils import get_root
 import os
 import ccxt
 import pandas as pd
 import numpy as np #ignore
 
+root = get_root()
 
-load_dotenv() #ladowanie kluczy api
+load_dotenv()
 api_key1 = str(os.environ.get('bitget_api_key'))
 secret_api_key1 = str(os.environ.get('bitget_secret_api_key'))
 symbol1 = "SUI/USDT:USDT"
@@ -14,7 +16,6 @@ exchange1 = "bitget"
 market_type1 = "FUTURES"
 
 def fetching_trades(symbol: str, exchange_id: str, market_type: str, api_key: str, secret_api_key: str):
-	#ustawienie gieldy oraz symbolu
 	chosen_symbol = symbol
 	chosen_exchange = getattr(ccxt, exchange_id)
 	exchange = chosen_exchange({
@@ -23,24 +24,20 @@ def fetching_trades(symbol: str, exchange_id: str, market_type: str, api_key: st
 		"enableRateLimit": True
 	})
 
-	#pobranie historycznych transakcji oraz przetransformowanie jej do DataFrame z pandas
 	trade = exchange.fetch_trades(chosen_symbol)
 	trades = pd.DataFrame(trade)
 	needed_df = trades[['datetime', 'price']]
 
-	#przeksztalcenie nazwy symbolu tak by mozna bylo zapisac plik oraz zapisanie pliku
 	first_name_change = chosen_symbol.replace('/', '_')
 	second_name_change = first_name_change.replace(':', '_')
 	correct_file_name = second_name_change.lower()
 
-	#zdefiniowanie nazwy folderu
 	if ":USDT" in first_name_change:
 		folder_name = first_name_change.replace(':USDT', '')
 	else:
 		folder_name = first_name_change
 
-	#zdefiniowanie sciezki folderu do zapisu pobranych danych i utworzenie brakujacego folderu
-	desired_path = f"../../Data/{folder_name}/{market_type}/{correct_file_name}_tickers.csv"
+	desired_path = f"/{root}/Data/{folder_name}/{market_type}/{correct_file_name}_tickers.csv"
 	file_path = Path(desired_path)
 	target_directory = file_path.parent
 	target_directory.mkdir(parents = True, exist_ok = True)
