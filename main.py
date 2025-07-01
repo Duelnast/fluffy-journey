516ff2f9aaba2d580717283e1e1655dcd2a0505b
Duelnast <logolenarczyk@gmail.com>
2025-07-01 00:07:55 +0000

    WIP: rozpoczecie pracy nad main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.logic import RenkoCalculator
+from src.utils import get_root
+import pandas as pd
+import numpy as np
+
+'''dodawac kazda funkcje z ktorej chce skorzystac i przy pomocy input() wybierac numerycznie ktora ma sie uruchomic
+np. 1-9, przy pomocy if statement bede w stanie uruchomic tylko konkretny skrypt.
+'''
