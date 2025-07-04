b54e7478185eaa928b1281b60d00c524dc7870e2
Duelnast <logolenarczyk@gmail.com>
2025-07-04 19:03:49 +0000

    Aktualizacja skryptu do dodawania danych do bazy danych

--- main.py
+++ main.py
@@ -6,33 +6,43 @@ import pandas as pd
 import numpy as np
 import os
 
+if __name__ == '__main__':
+	root = get_root()
+	hist_tickers_dir = f'{root}/Data/Ticker/Historic'
 
-root = get_root()
-hist_tickers_dir = f'{root}/Data/Ticker/Historic'
+	which_one = input('Which one script to run (1-n):')
+	if which_one == '1':
+		market_type = input('Which type of market you want to run (spot or futures):')
+		symbol = input('Which symbol do you want to run (eg. SUIUSDT):')
+		start_year = int(input('From which year do you want to run (eg. 2024):'))
+		end_year = int(input('To which year do you want to run (eg. 2025):'))
+		start_month = int(input('From which month do you want to run (01-12):'))
+		end_month = int(input('To which month do you want to run (01-12):'))
 
-which_one = input('Which one script to run (1-n):')
-if which_one == '1':
-	market_type = input('Which type of market you want to run (spot or futures):')
-	symbol = input('Which symbol do you want to run (eg. SUIUSDT):')
-	start_year = int(input('From which year do you want to run (eg. 2024):'))
-	end_year = int(input('To which year do you want to run (eg. 2025):'))
-	start_month = int(input('From which month do you want to run (01-12):'))
-	end_month = int(input('To which month do you want to run (01-12):'))
+		file_list = get_file_name(symbol, start_year, end_year, start_month, end_month)
+		full_file_list = [f'{hist_tickers_dir}/{market_type}/{file}' for file in file_list]
+		total_files = len(file_list)
+		print(f'Total files: {total_files}')
 
-	file_list = get_file_name(symbol, start_year, end_year, start_month, end_month)
-	total_files = len(file_list)
-	print(f'Total files: {total_files}')
+		print('Connecting to database...')
+		try:
+			db = DatabaseHandler()
+			print('Connection established...')
+		except Exception as e:
+			print(f'Error with connection: {e}')
+			exit()
 
-	print('Connecting to database...')
-	try:
-		db = DatabaseHandler()
-		print('Connection established...')
-	except Exception as e:
-		print(f'Eroor with connection: {e}')
-		exit()
+		def saving_into_db(file_path):
+			try:
+				for df_chunk in pd.read_csv(file_path, usecols=['time', 'price', 'qty', 'is_buyer_maker'], chunksize=500000):
+					df_chunk['symbol'] = symbol
+					db.append_df(df_chunk, "ticker_trades_futures")
 
-	for index, file in enumerate(file_list):
-		temp_df = pd.read_csv(f'{hist_tickers_dir}/{market_type}/{file}', usecols=['time', 'price', 'qty', 'is_buyer_maker'], chunksize=50000)
-		temp_df['symbol'] = symbol
-		db.append_df(temp_df, "trades")
-		print(f'{index}/{total_files}')
+				print(f'Thread {os.getpid()}: finished file {os.path.basename(file_path)}')
+			except Exception as e:
+				print(f'Error with the processing file {os.path.basename(file_path)}: {e}')
+
+		with ThreadPoolExecutor(max_workers=16) as executor:
+			executor.map(saving_into_db, full_file_list)
+
+		print('Finished! All files saved in database.')
